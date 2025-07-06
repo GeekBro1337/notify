01e3368e158d9b2716d48e81aee51858f7f4abac
GeekBro1337 <133227826+GeekBro1337@users.noreply.github.com>
2025-07-06 16:34:27 +0500

    Add Tailwind CSS and Nuxt UI

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <UNotifications />
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <UContainer class="py-10">
+    <h1 class="text-2xl font-bold mb-4">Notifications App</h1>
+    <UButton to="/" color="primary">Home</UButton>
+  </UContainer>
+</template>
+
+<script setup lang="ts">
+</script>
